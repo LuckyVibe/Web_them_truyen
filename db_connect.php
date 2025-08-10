f5cfc3406fa4adb432ba5b8e6cc00892f88201b1
LuckyVibe <ntny25042005@gmail.com>
2025-08-10 20:48:23 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql201.infinityfree.com"; // Tên máy chủ
-$username = "if0_39405731"; // Tên người dùng MySQL mặc định của XAMPP
-$password = "HiHikc012"; // Mật khẩu MySQL mặc định của XAMPP (trống)
-$dbname = "if0_39405731_my_truyen"; // Tên cơ sở dữ liệu bạn đã tạo
+$servername = "sql201.infinityfree.com"; // Tên máy chủ "localhost"
+$username = "if0_39405731"; // Tên người dùng MySQL mặc định của XAMPP "root"
+$password = "HiHikc012"; // Mật khẩu MySQL mặc định của XAMPP (trống) ""
+$dbname = "if0_39405731_my_truyen"; // Tên cơ sở dữ liệu bạn đã tạo "truyen online"
 
 // Tạo kết nối
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -14,4 +14,5 @@ if ($conn->connect_error) {
 
 // Đặt bộ ký tự cho kết nối
 $conn->set_charset("utf8mb4");
+
 ?>
